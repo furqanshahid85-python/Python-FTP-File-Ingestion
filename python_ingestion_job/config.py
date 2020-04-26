6faab0c65437488c6152195495c6b1827f9915f9
Furqan Butt <furqan.butt@northbaysolutions.net>
2020-04-26 22:29:26 +0500

    added python ingestion job for batch processing

---
+++ python_ingestion_job/config.py
@@ -0,0 +1,29 @@
+# paramiko parameters
+SSH_CLIENT = None
+SFTP_CLIENT = None
+SSH_OK = False
+SFTP_OK = False
+
+# ftp credentials
+FTP_HOST = "<ftp host ip>"
+FTP_PORT = "<ftp port>"
+FTP_USERNAME = "<ftp username>"
+FTP_PASSWORD = "<ftp password>"
+
+# s3 parameters
+S3_BUCKET = "<s3 bucket name>"
+
+# sftp directory paths
+PARENT_DIR_PATH = "<path of directory on FTP server where all the files are present>"
+ORIGINAL_DIR_PATH = "<path to directory named 'original' on FTP server>"
+PROCESSING_DIR_PATH = "<path to directory named 'processing' on FTP server>"
+PROCESSED_DIR_PATH = "<path to directory named 'processed' on FTP server>"
+
+# multipart parameters
+MB = 1024 ** 2
+GB = 1024 ** 3
+
+MULTIPART_THRESHOLD = 100 * MB
+MULTIPART_CHUNKSIZE=20 * MB
+MAX_CUNCURRENCY=10
+USER_THREADS=True
---
+++ python_ingestion_job/python_glue_injestion_job.py
@@ -0,0 +1,258 @@
+"""
+This module provides the functionality of uploading files to s3 from a FTP server. An SFTP connection is
+created with the FTP server and all the files present in the specified directory are uploaded to the specified
+s3 bucket.
+Following are the key features of this module:
+* Creates a secure ssh connection with FTP server.
+* Handles multipart upload to s3 automatically, if file size is greater than 100MB (can be configured).
+* Automatically handles retires in case of failed uploads during multipart upload.
+* Partitions the data in s3 based on current year,month,day,hour.
+* Ensures which file has been processed or needs to be processed.
+*
+*
+*
+*
+"""
+
+import os
+import boto3
+import config as cfg
+from boto3.s3.transfer import TransferConfig
+from datetime import datetime
+
+# uncomment following lines if running as an AWS Glue Job.
+
+# install_path = os.environ['GLUE_INSTALLATION']
+# easy_install.main( ["--install-dir", install_path, "https://files.pythonhosted.org/packages/ac/15/4351003352e11300b9f44a13576bff52dcdc6e4a911129c07447bda0a358/paramiko-2.7.1.tar.gz"] )
+# reload(site)
+
+import paramiko
+
+congfig_file = "config.json"
+
+
+class FTPIngestion:
+    
+    def __init__(self):
+        self.ssh_client = cfg.SSH_CLIENT
+        self.sftp_client = cfg.SFTP_CLIENT
+        self.ssh_ok = cfg.SSH_OK
+        self.sftp_ok = cfg.SFTP_OK
+        self.ftp_host = cfg.FTP_HOST
+        self.ftp_port = cfg.FTP_PORT
+        self.ftp_username = cfg.FTP_USERNAME
+        self.ftp_password = cfg.FTP_PASSWORD
+        self.s3 = boto3.client('s3')
+        self.s3_bucket_name = cfg.S3_BUCKET
+        self.ftp_directory_path = cfg.PARENT_DIR_PATH
+        self.ftp_original_path = cfg.ORIGINAL_DIR_PATH
+        self.ftp_processing_path = cfg.PROCESSING_DIR_PATH
+        self.ftp_processed_path = cfg.PROCESSED_DIR_PATH
+
+    def create_ssh_connection(self):
+        """
+        Creates SSH connection with FTP server. The authentication credentials are provided in the config file.
+        Can also use AWS SSM instead of Config file.
+        :return : Bool; True if connection successfull, False if not
+        """
+
+        print('Establishing connection with host...')
+        try:
+            self.ssh_client = paramiko.SSHClient()
+            self.ssh_client.set_missing_host_key_policy(
+                paramiko.AutoAddPolicy())
+
+            # in production, use load_system_host_keys
+            # ssh_client.load_system_host_keys()
+
+            self.ssh_client.connect(hostname=self.ftp_host, username=self.ftp_username,
+                                    password=self.ftp_password, port=self.ftp_port)
+            print('connected')
+            self.ssh_ok = True
+
+        except paramiko.AuthenticationException as AuthFailException:
+            self.ssh_ok = False
+            print('Authentication Failed, error: ', str(AuthFailException))
+        except paramiko.SSHException as sshException:
+            self.ssh_ok = False
+            print('Could not establish ssh connection, error: ', str(sshException))
+        except Exception as error:
+            self.ssh_ok = False
+            print('Error establishing connection, error: ',
+                  str(AuthFailException))
+
+        return self.ssh_ok
+
+    def create_sftp_connection(self):
+        """
+        Creates an SFTP connection from the ssh connection created wiht the FTP server.
+        :return : Bool; True if connection successful, False if not.
+        """
+        try:
+            if self.create_ssh_connection():
+                print('Establishing SFTP connection...')
+                self.sftp_client = self.ssh_client.open_sftp()
+                print('SFTP connection successfull')
+                self.sftp_ok = True
+            else:
+                print('Could not establish ssh connection')
+
+        except paramiko.SFTPError as sftpError:
+            self.sftp_ok = False
+            print('could not establish sftp connection, error: ', str(sftpError))
+        except Exception as error:
+            self.sftp_ok = False
+            print('could not establish sftp connection')
+        return self.sftp_ok
+
+    def move_files_to_original(self):
+        """
+        This method copies the original files from parent directory to original directory on the FTP server.
+        :param source_file_path: directory path in which file resides
+        :param source_file_name: name of the file
+        """
+        print('moving files to original directory')
+        src = self.ftp_directory_path + '/*'
+        dest = self.ftp_original_path
+
+        try:
+            stdin, stdout, stderr = self.ssh_client.exec_command(
+                "mv " + src+" " + dest)
+        except Exception as error:
+            print("error moving files to original directory, error: ", error)
+
+    def move_files_to_processing(self, source_file_path, source_file_name):
+        """
+        This method copies the original files from parent directory to processing directory on the FTP server.
+        :param source_file_path: directory path in which file resides
+        :param source_file_name: name of the file
+        """
+
+        print('moving '+source_file_name+' to processing directory.')
+        processing_directory = self.ftp_processing_path
+        src = source_file_path+'/' + source_file_name
+        dest = processing_directory + source_file_name
+        try:
+            stdin, stdout, stderr = self.ssh_client.exec_command(
+                "cp " + src+" " + dest)
+        except Exception as error:
+            print("error moving files to proccessing directory, error: ", error)
+
+    def move_files_to_processed(self, source_file_path, source_file_name):
+        """
+        This method copies the original files from parent directory to processing directory on the FTP server.
+        :param source_file_path: directory path in which file resides
+        :param source_file_name: name of the file
+        """
+
+        print('moving '+source_file_name+' to processed directory.')
+        processed_directory = self.ftp_processed_path
+        src = source_file_path+'/' + source_file_name
+        dest = processed_directory + source_file_name
+
+        try:
+            stdin, stdout, stderr = self.ssh_client.exec_command(
+                "mv " + src+" " + dest)
+        except Exception as error:
+            print("error moving files to processed directory, error: ", error)
+
+    def create_s3_partition(self):
+        """
+        This method develops the partition string that will be used when uploading to the s3. The files are
+        uploaded to the specified partition.
+        The partition structure is as follows:
+        /<directory_name>/year = <year>/ month = <month>/ day = <day>/ hour = <hour>/file
+
+        """
+        parent_dir = self.ftp_directory_path.split("/")[-1]
+        current_date = datetime.now()
+        current_year = str(current_date.year)
+        current_month = str(current_date.month)
+        current_day = str(current_date.day)
+        current_hour = str(current_date.hour)
+
+        s3_partition = parent_dir + '/year = ' + current_year + '/month = ' + \
+            current_month + '/day = ' + current_day + '/hour = ' + current_hour + '/'
+        return s3_partition
+
+    def s3_upload_file_multipart(self, source_file, s3_target_file):
+        """
+        This method uploads file to s3. If file size is greater than 100MB then file gets uploaded via
+        multipart. The retires are handed automatically in case of failure. If file is less than 100MB in size
+        then it gets uploaded in regular manner. Each part of the file during multipart upload would be 50MB in
+        size.
+        Following parameters are configurable in Config File based on user needs:
+
+        multipart_threshold : 100MB (default)
+        multipart_chunksize : 20 MB (default)
+        max_concurrency : 10 (default)
+        user_threads : True (default)
+
+        :param source_file: the file object that is to be uploaded.
+        :param s3_target_file: the object in s3 bucket.
+
+        """
+        try:
+            config = TransferConfig(
+                multipart_threshold=cfg.MULTIPART_THRESHOLD, multipart_chunksize=cfg.MULTIPART_CHUNKSIZE,
+                max_concurrency=cfg.MAX_CUNCURRENCY, use_threads=cfg.USER_THREADS)
+
+            self.s3.upload_fileobj(source_file, self.s3_bucket_name,
+                                   s3_target_file, Config=config)
+        except Exception as error:
+            print('could not upload file using multipart upload, error: ', error)
+
+    def initiate_ingestion(self):
+        """
+        This method initiates the calls to establish ssh and sftp connections. Changes FTP directory path to
+        specified path. Gets list of all the files in the FTP specified path and starts upload to s3. Once all
+        files are uploaded closes all the connections. Ensures which file has been processed or needs to be
+        processed.
+        """
+        try:
+            if self.create_sftp_connection():
+                # copy files from parent directory to original directory
+                self.move_files_to_original()
+                self.sftp_client.chdir(self.ftp_original_path)
+
+                # copy files from parent directory to processing directory
+                files_to_upload = self.sftp_client.listdir()
+                for ftp_file in files_to_upload:
+                    self.move_files_to_processing(
+                        self.sftp_client.getcwd(), ftp_file)
+
+                # upload files in processing dir to s3
+                self.sftp_client.chdir(self.ftp_processing_path)
+                files_in_processing = self.sftp_client.listdir()
+                s3_partition = self.create_s3_partition()
+
+                for ftp_file in files_in_processing:
+                    sftp_file_obj = self.sftp_client.file(ftp_file, mode='r')
+                    self.s3_upload_file_multipart(
+                        sftp_file_obj, s3_partition+ftp_file)
+                print('all files uploaded to s3')
+
+                # move files from processing dir to processed dir
+                files_to_move = self.sftp_client.listdir()
+                for ftp_file in files_to_move:
+                    self.move_files_to_processed(
+                        self.sftp_client.getcwd(), ftp_file)
+            else:
+                print('Could not establish SFTP connection')
+        except Exception as error:
+            print("files ingestion failed, error: ", str(error))
+
+        self.close_connections()
+
+    def close_connections(self):
+        """
+        This mehtod is used to close the ssh and sftp connections.
+        """
+        print('closing connections')
+        self.sftp_client.close()
+        self.ssh_client.close()
+
+
+if __name__ == "__main__":
+    ftp_obj = FTPIngestion()
+    ftp_obj.initiate_ingestion()
